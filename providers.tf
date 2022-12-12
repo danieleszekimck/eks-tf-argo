be40237b2dce1648bd0934d2214fc3c21ed481d5
daniel_eszeki <daniel_eszeki@mckinsey.com>
2022-12-12 15:38:14 +0100

    added tf files

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "ekspoc"
+}
